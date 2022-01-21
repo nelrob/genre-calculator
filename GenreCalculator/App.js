6734a422df905142f142ccd6fcbff70c0281e597
nelrob <71628453+nelrob@users.noreply.github.com>
2022-01-21 17:21:29 +0400

    Reorder UI

--- GenreCalculator/App.js
+++ GenreCalculator/App.js
@@ -1,112 +1,151 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useState, useEffect} from 'react';
+//import SplashScreen from 'react-native-splash-screen';
+import {TouchableOpacity, View, Text, StyleSheet, Image, Modal, Button} from 'react-native';
+import {styles} from './styles/styles'
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+// need to have a function where the image opacity goes 40% when button is pressed to signify that data is stored 
+// const isPressed = false;
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// const imageButton = () => ({
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+// })
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+function App() {
+  const [modalOpen, setModalOpen] = useState(true)
+ // const [text, setText] = useState("+");
+  // place in onSecondImageClick
+  // const onPressHandler = event => setText("Calculate");
+  
+  // splash screen thign idk
+  //useEffect(() => {SplashScreen.hide()})
+  // need function to store image value for image onClick
+    return(
+      <View style={styles.container}>
+        <View style={styles.topContainer} elevation={5}>
+          <TouchableOpacity 
+            style={{    
+              borderWidth:1,
+              alignItems:'center',
+              justifyContent:'center',
+              width:40,
+              height:40,
+              backgroundColor:'#fff',
+              borderRadius:50,
+              marginTop: 20,}}
+              onPress={() => setModalOpen (true)}>
+            <Text>?</Text>
+          </TouchableOpacity>
+          <Text style={{fontSize: 40,margin:10,}}>Genre Calculator</Text>
+          <Text style={{fontSize: 13, marginTop:20}}>Calculate two genres and receive a hybrid recommendation!</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+        
+        {/* images 
+        onClick = needs to store their image value
+        example: pop = 1, rock = 2, etc
+        */}
+        <Text>First Genre</Text>
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+        <View style={styles.buttonGroup}>
+          <View style={styles.buttonRow}>
+            <TouchableOpacity>     
+            <Text style ={styles.buttonText}> Pop</Text>     
+              <Image source={require("./assets/pop.png")} style={styles.buttonImage}/>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.buttonRow}>
+            <TouchableOpacity>          
+              <Image source={require("./assets/hiphop.png")} style={styles.buttonImage}/>
+              <Text style ={styles.buttonText}> Pop</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.buttonRow}>
+            <TouchableOpacity>          
+              <Image source={require("./assets/alt.png")} style={styles.buttonImage}/>
+              <Text style ={styles.buttonText}> Pop</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.buttonRow}>
+            <TouchableOpacity> 
+              <Text style ={styles.buttonText}> Pop</Text>         
+              <Image source={require("./assets/rock.png")} style={styles.buttonImage}/>
+            </TouchableOpacity>
+          </View>
+          
+        </View>
+        
+        <Text>Second Genre</Text>
+
+        <View style={styles.buttonGroup}>
+          <View style={styles.buttonRow}>
+            <TouchableOpacity>     
+            <Text style ={styles.buttonText}> Pop</Text>     
+              <Image source={require("./assets/pop.png")} style={styles.buttonImage}/>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.buttonRow}>
+            <TouchableOpacity>          
+              <Image source={require("./assets/hiphop.png")} style={styles.buttonImage}/>
+              <Text style ={styles.buttonText}> Pop</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.buttonRow}>
+            <TouchableOpacity>          
+              <Image source={require("./assets/alt.png")} style={styles.buttonImage}/>
+              <Text style ={styles.buttonText}> Pop</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.buttonRow}>
+            <TouchableOpacity> 
+              <Text style ={styles.buttonText}> Pop</Text>         
+              <Image source={require("./assets/rock.png")} style={styles.buttonImage}/>
+            </TouchableOpacity>
+          </View>
+          
+        </View>
+
+        {/* // calculate button */}
+        {/* onpress: show calculated genre */}
+        <View>
+          <TouchableOpacity>
+            <Text style={{
+              paddingBottom: 80,
+              fontSize: 50,
+              borderStyle: "dashed",
+              alignSelf: "center",
+            }}>Calculate!</Text>
+          </TouchableOpacity>
+
+        </View>
+
+              {/*POPUP MODAL FOR FIRST TIME */}
+        <Modal
+        animationType="slide"
+        visible={modalOpen}r
+        style={{flex: 0.5,
+          backgroundColor: 'rgba(0,0,0,0.5)',
+          justifyContent: 'center',
+          alignSelf: 'center',}}
+        >
+          <View style={styles.modalContainer}>
+
+          <TouchableOpacity onPress={() => setModalOpen (false)}>
+            {/* close button
+              should reset all genre values and text values */}
+            <Text>O</Text>
+          </TouchableOpacity>
+
+            <Text style={styles.modalText}>Welcome to Genre Calculator!</Text>
+          </View>
+        </Modal>
+
+      </View>
+    )
+}
 
 export default App;
---
+++ GenreCalculator/styles/styles.js
@@ -0,0 +1,56 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e5e5e5',
+    fontFamily: 'NexaRegular',
+  },
+  topContainer: {
+    flex: 0.3,
+    backgroundColor: 'red',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    marginTop: 10,
+    paddingBottom: 10,
+
+  },
+  
+  buttonText: {
+    flex: 1,
+    alignSelf: "center",
+  },
+  buttonGroup: {
+    flex: 0.2,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    backgroundColor: 'pink',
+    flexWrap: "wrap",
+    paddingHorizontal: 3,
+    paddingVertical: 3,
+  },
+  buttonImage: {
+    alignItems:'center',
+    justifyContent:'center',
+    width:60,
+    height:60,
+  },
+  buttonRow: {
+    backgroundColor: "white",
+    marginVertical: 10,
+    borderRadius: 50,
+  },
+  modalContainer: {
+    flex:1,
+    backgroundColor: 'transparent',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderRadius: 20,
+    elevation: 20,
+  },
+  modalText:{
+
+  }
+});
